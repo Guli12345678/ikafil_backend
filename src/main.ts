c252a127bd5b3510a0dcccce26e4afd2a1563d24
Guli12345678 <gulsanammamadieva083@gmail.com>
2025-11-01 20:03:13 +0500

    fixed

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,16 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
 
   app.enableCors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://ikafil.uz", "https://ikafil-admin-dash.vercel.app", "https://ikafil-zeta.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://ikafil.uz",
+      "https://ikafil-admin-dash.vercel.app",
+      "https://ikafil-two.vercel.app",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-  })
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
