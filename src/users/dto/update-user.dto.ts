f886cb354671dc35e31d14b95b0afa4ba9ff21bf
Guli12345678 <gulsanammamadieva083@gmail.com>
2025-11-01 20:18:10 +0500

    last

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,11 +2,8 @@ import { PartialType, OmitType } from "@nestjs/swagger";
 import { CreateUserDto } from "./create-user.dto";
 
 export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, [
-    "password",
-    "confirmPassword",
-    "email",
-  ] as const)
+  OmitType(CreateUserDto, ["password", "confirmPassword"] as const)
 ) {
+  email?: string;
   isActive?: boolean;
 }
